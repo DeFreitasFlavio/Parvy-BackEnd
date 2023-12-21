97ef67ba82ad42bce70d3eb6fcb2d7f2d445bea4
Hugo Lasserre <hugo.lasserrecab@gmail.com>
2023-12-21 11:13:41 +0100

    start game

--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -4,146 +4,189 @@ import type { CacheIORedis } from 'src/app.module';
 import { Card, cardSchema } from 'src/models/card.model';
 import { Player, playerSchema } from 'src/models/player.model';
 import { Pyramid, pyramidSchema } from 'src/models/pyramid.model';
+import IORedis from 'ioredis';
+
+export interface GameSettings {
+  etages: number;
+  nbCardsInHand?: number;
+}
+
+type GameParams = Required<GameSettings> & { code: string };
 
 @Injectable()
 export class GameService {
-  constructor(@Inject(CACHE_MANAGER) private cacheManager: CacheIORedis) {}
+  private client: IORedis.Redis | IORedis.Cluster;
 
-    async startGame(code: string, etages: number): Promise<{}> {
-        this.createCards(code);
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: CacheIORedis) {
+    this.client = this.cacheManager.store.getClient();
+  }
 
-        const client = this.cacheManager.store.getClient();
+  async startGame(code: string, settings: GameSettings): Promise<void> {
+    const params: GameParams = {
+      nbCardsInHand: 4,
+      ...settings,
+      code,
+    };
 
-        let cards: Card[] = [];
-        let players: Player[] = [];
-        let playerCard: Card[] = [];
-        const nbCardsInHand = 4;
-        let pyramide: Pyramid = {
-            'totalLevels': etages
-        };
+    await this.createCards(code);
 
-        //Récupération des cartes dans un tableau
-        for (let i = 1; i <= 52; i++) {
-            const card = cardSchema.parse(await client.hgetall(code + '/card/' + i));
-            cards.push(card);
-        }
+    const playerIds = await this.client.smembers(
+      code + '/players',
+    ); /* liste des id players */
 
-        //Mélange des cartes dans la pioche
-        for (let i = 0; i < cards.length - 1; i++) {
-            const j = (i + Math.floor(Math.random() * (cards.length - i - 1))) % cards.length;
-            [cards[i], cards[j]] = [cards[j], cards[i]];
-        }
+    await this.dealHands(playerIds, params);
 
-        const playerList = await client.smembers(code + '/players'); /* liste des id players */
-        const nbPlayersInRoom = playerList.length; /* nombre de joueurs dans la room */
+    await this.buildPyramid(playerIds, params);
+  }
 
-        //Pour chaque joueur dans la room retirer des cartes de la pioche et lui donner les cartes dans la main
-        for (let i = 0; i < nbPlayersInRoom; i++) {
-            const player = playerSchema.parse(await client.hgetall(playerList[i]));
-            console.log(player);
-            //Pour chaque carte à mettre dans la main
-            for (let j = 0; j < nbCardsInHand; j++) {
-                const currentCard = cards.pop();
-                if (!currentCard) {
-                    throw new Error('No card left to deal');
-                }
-                await client.hset(code+'/players/'+player.id+'/hand/card/'+currentCard.id, currentCard);
-                await client.del(code+'/card/'+currentCard.id);
-                playerCard.push(currentCard);
-            }
-            // Trier le tableau playerCard
-            playerCard.sort((a, b) => Number(a.id) - Number(b.id));
+  async dealHands(playerIds: string[], { code, nbCardsInHand }: GameParams) {
+    // LIST $code/deck
+    // LIST $code/players/$playerId/hand
 
-            //Ajouter la main du joueur au joueur
-            Object.assign(player, { "hand": playerCard });
-            players.push(player);
-        }
+    //Pour chaque joueur dans la room retirer des cartes de la pioche et lui donner les cartes dans la main
+    const nbOfCardToDraw = nbCardsInHand * playerIds.length;
+    const cardsToDeal = await this.client.lpop(`${code}/deck`, nbOfCardToDraw);
 
-        let nbCardsInPyramid = 0;
-        for (let i = 1; i <= etages; i++) {
-            nbCardsInPyramid += i;
-        }
+    const cardsToPlayers = playerIds.map((playerId) => ({
+      playerId,
+      hand: cardsToDeal.splice(nbCardsInHand),
+    }));
 
-        if (cards.length - players.length * nbCardsInHand + nbCardsInPyramid <= 0) {
-            throw new Error('Number of floors to big');
-        }
+    const promises: Promise<unknown>[] = [];
+    for (const { playerId, hand } of cardsToPlayers) {
+      const toSort = hand.map((stringifiedCard) => ({
+        pos: JSON.parse(stringifiedCard).id,
+        stringifiedCard,
+      }));
+      toSort.sort((a, b) => a.pos - b.pos);
+      const sortedHand = toSort.map((card) => card.stringifiedCard);
 
-        //Création de la pyramide. Pour chaque étage, attribuer un nombre de carte
-        for (let numEtage = etages; numEtage > 0; numEtage--) {
-            //Récupérer le nombre de carte pour les mettre dans la pyramide
-            const cardsInFloor: Card[] = [];
-            for (let nbCardsInFloor = numEtage; nbCardsInFloor > 0; nbCardsInFloor--) {
-                const currentCard = cards.pop();
-                if (!currentCard) {
-                    throw new Error('No card left to deal pyramid');
-                }
-                cardsInFloor.push(currentCard);
-                await client.del(code+'/card/'+currentCard.id);
-            }
-            const nameObjectCurrentFloor = `floor${numEtage}`;
-            Object.assign(pyramide, {
-                [nameObjectCurrentFloor]: {
-                    'level': numEtage,
-                    'cards': cardsInFloor
-                }
-            })
-        }
-
-        let response = {
-            response: 'ok',
-            cards,
-            players,
-            pyramide
-        }
-
-        return response;
+      promises.push(
+        this.client.lpush(`${code}/players/${playerId}/hand`, sortedHand),
+      );
     }
 
-    private async createCards(code: string) {
-        const client = this.cacheManager.store.getClient();
+    await Promise.all(promises);
+  }
 
-        const colors = ['red', 'black'];
-        const blackSignes = ['pique', 'trefle'];
-        const redSignes = ['carreau', 'coeur'];
-        const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-        let idCard = 1;
+  async buildPyramid(
+    playerIds: string[],
+    { code, nbCardsInHand, etages }: GameParams,
+  ) {
+    let pyramide: Pyramid = {
+      totalLevels: etages,
+    };
 
-        for (let color in colors) {
-            //Générer les cartes rouges
-            if (colors[color] === 'red') {
-                for (let signe in redSignes) {
-                    for (let value in values) {
-                        await client.hset(
-                            code + '/card/' + idCard, 
-                            'id', idCard, 
-                            'value', values[value],
-                            'signe', redSignes[signe],
-                            'color', colors[color],
-                            'face', 0,
-                            'image', redSignes[signe]+'-'+values[value]+'.png'
-                        );
-                        idCard += 1;
-                    }
-                }
-            }
-
-            //Générer les cartes noires
-            if (colors[color] === 'black') {
-                for (let signe in blackSignes) {
-                    for (let value in values) {
-                        await client.hset(
-                            code + '/card/' + idCard, 
-                            'id', idCard, 
-                            'value', values[value],
-                            'signe', redSignes[signe],
-                            'color', colors[color],
-                            'face', 0,
-                            'image', redSignes[signe]+'-'+values[value]+'.png'
-                        );
-                        idCard += 1;
-                    }
-                }
-            }
-        }
+    let nbCardsInPyramid = 0;
+    for (let i = 1; i <= etages; i++) {
+      nbCardsInPyramid += i;
     }
+
+    const deckKey = `${code}/deck`;
+
+    const remainingCardLength = await this.client.llen(deckKey);
+
+    if (
+      remainingCardLength -
+        playerIds.length * nbCardsInHand +
+        nbCardsInPyramid <=
+      0
+    ) {
+      throw new Error('Number of floors to big');
+    }
+
+    // 1. lmpop nbCardsInPyramid in deck
+    // 2. construire un tableau par etage avec les cartes
+    // 3. persister chaque etage avec un promise all
+
+    // 30. optimiser en utilisant les fontionalité de transaction et d'execution multiple à redis
+
+    //Création de la pyramide. Pour chaque étage, attribuer un nombre de carte
+    for (let numEtage = etages; numEtage > 0; numEtage--) {
+      //Récupérer le nombre de carte pour les mettre dans la pyramide
+      const cardsInFloor: Card[] = [];
+      for (
+        let nbCardsInFloor = numEtage;
+        nbCardsInFloor > 0;
+        nbCardsInFloor--
+      ) {
+        const currentCard = cards.pop();
+        if (!currentCard) {
+          throw new Error('No card left to deal pyramid');
+        }
+        cardsInFloor.push(currentCard);
+        await this.client.del(code + '/card/' + currentCard.id);
+      }
+      const nameObjectCurrentFloor = `floor${numEtage}`;
+      Object.assign(pyramide, {
+        [nameObjectCurrentFloor]: {
+          level: numEtage,
+          cards: cardsInFloor,
+        },
+      });
+    }
+
+    let response = {
+      response: 'ok',
+      cards,
+      players,
+      pyramide,
+    };
+
+    return response;
+  }
+
+  private async createCards(code: string) {
+    const cardKinds = [
+      { color: 'red', signe: 'carreau' },
+      { color: 'red', signe: 'coeur' },
+      { color: 'black', signe: 'trefle' },
+      { color: 'black', signe: 'pique' },
+    ];
+    const values = [
+      'A',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      '10',
+      'J',
+      'Q',
+      'K',
+    ];
+    let id = 1;
+
+    const deck = [];
+
+    for (let { color, signe } of cardKinds) {
+      for (let value of values) {
+        deck.push({
+          id,
+          value,
+          signe,
+          color,
+          face: 0,
+          image: `${signe}-${value}.png`,
+        });
+
+        id += 1;
+      }
+    }
+
+    //Mélange des cartes dans la pioche
+    for (let i = 0; i < deck.length - 1; i++) {
+      const j =
+        (i + Math.floor(Math.random() * (deck.length - i - 1))) % deck.length;
+      [deck[i], deck[j]] = [deck[j], deck[i]];
+    }
+
+    await this.client.lpush(
+      `${code}/deck`,
+      deck, // or deck.map((card) => JSON.stringify(card))
+    );
+  }
 }
